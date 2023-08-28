fe9a2246593cbfce1a1c735207005dd4d240a2fc
Valeri <valeri.torishnya@gmail.com>
2023-08-28 18:14:01 +0300

    create component Result

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
-import CalculateForm from "./components/CalculateForm";
 import Header from "./components/Header";
+import Result from "./components/Result";
+import CalculateForm from "./components/CalculateForm";
 
 
 function App() {
@@ -38,26 +39,7 @@ function App() {
       {/* Todo: Show below table conditionally (only once result data is available) */}
       {/* Show fallback text if no data is available */}
 
-      <table className="result">
-        <thead>
-          <tr>
-            <th>Year</th>
-            <th>Total Savings</th>
-            <th>Interest (Year)</th>
-            <th>Total Interest</th>
-            <th>Invested Capital</th>
-          </tr>
-        </thead>
-        <tbody>
-          <tr>
-            <td>YEAR NUMBER</td>
-            <td>TOTAL SAVINGS END OF YEAR</td>
-            <td>INTEREST GAINED IN YEAR</td>
-            <td>TOTAL INTEREST GAINED</td>
-            <td>TOTAL INVESTED CAPITAL</td>
-          </tr>
-        </tbody>
-      </table>
+      <Result />
     </div>
   );
 }
