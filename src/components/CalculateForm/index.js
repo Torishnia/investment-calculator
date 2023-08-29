63ef6dac098dbf55e6908fd4fb56726a616b0fb6
Valeri <valeri.torishnya@gmail.com>
2023-08-29 12:26:59 +0300

    add some logic

--- src/components/CalculateForm/index.js
+++ src/components/CalculateForm/index.js
@@ -1,16 +1,58 @@
+import { useState } from 'react';
 import styles from './index.module.scss';
 
-const CalculateForm = () => {
+const initialUserInput = {
+  currentSaving: 10000,
+  yearlyContribution: 1200,
+  expectedReturn: 7,
+  duration: 10,
+}
+
+const CalculateForm = ({ onCalculate }) => {
+  const [userInput, setUserInput] = useState(initialUserInput);
+
+  const submitHandler = (event) => {
+    event.preventDefault();
+    onCalculate(userInput);
+  }
+
+  const resetHandler = () => {
+    setUserInput(initialUserInput);
+  }
+
+  const inputChangeHandler = (identifier, value) => {
+    setUserInput((prevInput) => {
+      return {
+        ...prevInput,
+        [identifier]: +value,
+      }
+    })
+  }
+
   return (
-    <form className={styles.form}>
+    <form className={styles.form} onSubmit={submitHandler}>
       <div className={styles.input_group}>
         <p>
           <label htmlFor="current-savings">Current Savings ($)</label>
-          <input type="number" id="current-savings" />
+          <input 
+            type="number" 
+            id="current-savings"
+            value={userInput.currentSaving}
+            onChange={(event) => 
+              inputChangeHandler('currentSaving', event.target.value)
+            }
+          />
         </p>
         <p>
           <label htmlFor="yearly-contribution">Yearly Savings ($)</label>
-          <input type="number" id="yearly-contribution" />
+          <input 
+            type="number" 
+            id="yearly-contribution"
+            value={userInput.yearlyContribution}
+            onChange={(event) => 
+              inputChangeHandler('yearlyContribution', event.target.value)
+            }
+          />
         </p>
       </div>
       <div className={styles.input_group}>
@@ -18,15 +60,29 @@ const CalculateForm = () => {
           <label htmlFor="expected-return">
             Expected Interest (%, per year)
           </label>
-          <input type="number" id="expected-return" />
+          <input 
+            type="number" 
+            id="expected-return"
+            value={userInput.expectedReturn}
+            onChange={(event) => 
+              inputChangeHandler('expectedReturn', event.target.value)
+            }
+          />
         </p>
         <p>
           <label htmlFor="duration">Investment Duration (years)</label>
-          <input type="number" id="duration" />
+          <input 
+            type="number" 
+            id="duration"
+            value={userInput.duration}
+            onChange={(event) => 
+              inputChangeHandler('duration', event.target.value)
+            }
+          />
         </p>
       </div>
       <p className={styles.actions}>
-        <button type="reset" className={styles.buttonAlt}>
+        <button type="reset" className={styles.buttonAlt} onClick={resetHandler}>
           Reset
         </button>
         <button type="submit" className={styles.button}>
---
+++ src/components/ResultsTable/index.js
@@ -0,0 +1,46 @@
+import styles from './index.module.scss';
+
+const formatter = new Intl.NumberFormat('en-US', {
+  style: 'currency',
+  currency: 'USD',
+  minimumFractionDigits: 2,
+  maximumFractionDigits: 2,
+});
+
+const ResultsTable = ({ data, initialInvestment }) => {
+  return (
+    <div>
+      <table className={styles.result}>
+        <thead>
+          <tr>
+            <th>Year</th>
+            <th>Total Savings</th>
+            <th>Interest (Year)</th>
+            <th>Total Interest</th>
+            <th>Invested Capital</th>
+          </tr>
+        </thead>
+        <tbody>
+          {data.map((yearData) => (
+            <tr key={yearData.year}>
+              <td>{formatter.format(yearData.year)}</td>
+              <td>{formatter.format(yearData.savingsEndOfYear)}</td>
+              <td>{formatter.format(yearData.yearlyInterest)}</td>
+              <td>
+                {formatter.format(yearData.savingsEndOfYear - 
+                  initialInvestment -
+                  yearData.yearlyContribution * yearData.year)}
+              </td>
+              <td>
+                {formatter.format(initialInvestment + 
+                  yearData.yearlyContribution * yearData.year)}
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
+  )
+}
+
+export default ResultsTable;
